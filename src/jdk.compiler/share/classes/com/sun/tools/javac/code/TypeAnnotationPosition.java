315ad27915d3eed70c639517cc292057f9811e94
Yuri Nesterenko <yan@openjdk.org>
2022-07-29 12:16:00 +0000

    8268894: forged ASTs can provoke an AIOOBE at com.sun.tools.javac.jvm.ClassWriter::writePosition
    Backport-of: a9188f237ec23d4ca2a172e9a7897cb6e2b69857

--- src/jdk.compiler/share/classes/com/sun/tools/javac/code/TypeAnnotationPosition.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/code/TypeAnnotationPosition.java
@@ -295,7 +295,6 @@ public class TypeAnnotationPosition {
 
     public void updatePosOffset(int to) {
         offset = to;
-        lvarOffset = new int[]{to};
         isValidOffset = true;
     }
 
